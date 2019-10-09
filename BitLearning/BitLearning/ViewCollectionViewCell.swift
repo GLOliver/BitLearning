11002529a565fea4a502131a32276e3cefee04e3
Aluno Mack <alunomack@10-44-48-59.0200.mack.br.iosda.org>
2019-10-09 11:36:26 -0300

    Erro de versão

---
+++ BitLearning/BitLearning/ViewCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ViewCollectionViewCell.swift
+//  BitLearning
+//
+//  Created by Aluno Mack on 09/10/2019.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
